93a111c85ef2e044b31b469037be20c80921af76
rohit1302 <rohitnew1302@gmail.com>
2026-01-30 23:43:12 +0530

    reverse a string by recurssion

---
+++ reversestring.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int left = 0;
+        int right = s.size() - 1;
+
+        while (left < right) {
+            swap(s[left], s[right]);
+            left++;
+            right--;
+        }
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<char> s = {'h', 'e', 'l', 'l', 'o'};
+    
+    sol.reverseString(s);
+    
+    for (char c : s) {
+        cout << c;
+    }
+    cout << endl;
+    
+    return 0;
+}    
