9476325c49177336f9c6c73f76ec60e58327ea94
rohit1302 <rohitnew1302@gmail.com>
2026-01-30 23:39:05 +0530

    recurssion easy sum of digits

---
+++ sumofdigit.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+
+int sumOfDigits(int n) {
+    if (n == 0) return 0;   // Base case
+    return (n % 10) + sumOfDigits(n / 10);
+}
+
+int main() {
+    int num = 12345;
+    cout << "Sum of digits of " << num << " is: " << sumOfDigits(num) << endl;
+    return 0;
+}
