7920c0f09bc330c960cceab887bfd71573edbc1f
rohit1302 <rohitnew1302@gmail.com>
2026-01-31 23:49:42 +0530

    leetcode qsn121 stock exchange

---
+++ lc121.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minPrice = INT_MAX;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            minPrice = min(minPrice, price);
+            maxProfit = max(maxProfit, price - minPrice);
+        }
+
+        return maxProfit;
+    }
+};
