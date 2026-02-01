9c3681a8b10d875f4bcd4c5e5abf0c25ec5bf01b
rohit1302 <rohitnew1302@gmail.com>
2026-02-01 23:55:55 +0530

    leetcode qsn141 linkdlist cycle

---
+++ lc141.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+bool hasCycle(ListNode *head) {
+    if (!head) return false;
+
+    ListNode* slow = head;
+    ListNode* fast = head;
+
+    while (fast && fast->next) {
+        slow = slow->next;          // move 1 step
+        fast = fast->next->next;    // move 2 steps
+        if (slow == fast) return true;
+    }
+    return false;
+}
+
+int main() {
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+
+    // Create cycle: 3 -> 2
+    head->next->next->next = head->next;
+
+    if (hasCycle(head))
+        cout << "Cycle detected";
+    else
+        cout << "No cycle";
+}
