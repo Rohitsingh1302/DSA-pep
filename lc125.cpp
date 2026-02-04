127de6ad2430ec7586bd86b9894f8271c3fd4139
rohit1302 <rohitnew1302@gmail.com>
2026-02-04 23:57:44 +0530

    valid palindrome

---
+++ lc125.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+bool isPalindrome(string s) {
+    int left = 0, right = s.length() - 1;
+
+    while (left < right) {
+        while (left < right && !isalnum(s[left])) left++;
+        while (left < right && !isalnum(s[right])) right--;
+
+        if (tolower(s[left]) != tolower(s[right]))
+            return false;
+
+        left++;
+        right--;
+    }
+    return true;
+}
+
+int main() {
+    string s = "A man, a plan, a canal: Panama";
+
+    if (isPalindrome(s))
+        cout << "Palindrome";
+    else
+        cout << "Not Palindrome";
+
+    return 0;
+}
