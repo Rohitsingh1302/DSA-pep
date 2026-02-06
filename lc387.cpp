b8034cf2f77173bad3072b12a459300647d7f6a1
rohit1302 <rohitnew1302@gmail.com>
2026-02-06 23:13:02 +0530

    first unique chrctr

---
+++ lc387.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int firstUniqChar(string s) {
+    int freq[26] = {0};
+    for (char c : s) freq[c - 'a']++;
+    for (int i = 0; i < s.size(); i++)
+        if (freq[s[i] - 'a'] == 1) return i;
+    return -1;
+}
+
+int main() {
+    string s;
+    cin >> s;
+    cout << firstUniqChar(s);
+    return 0;
+}
