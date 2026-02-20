2486b38161141b516484c2289bab90facf065dcf
rohit1302 <rohitnew1302@gmail.com>
2026-02-20 13:58:12 +0530

    Maximum Product of Three Numbers

---
+++ lc628.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int maximumProduct(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+    int n = nums.size();
+
+    return max(nums[n-1] * nums[n-2] * nums[n-3],
+               nums[0] * nums[1] * nums[n-1]);
+}
+
+int main() {
+    vector<int> nums = {1, 2, 3, 4};
+    cout << maximumProduct(nums);
+    return 0;
+}
