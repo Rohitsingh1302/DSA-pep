e96fd938c4a67a08c8d550e7249d57d50b271f54
rohit1302 <rohitnew1302@gmail.com>
2026-03-06 23:59:38 +0530

    Subarray Sum Equals K

---
+++ lc560.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
+        mp[0] = 1;
+
+        int sum = 0, count = 0;
+
+        for(int x : nums) {
+            sum += x;
+
+            if(mp.find(sum - k) != mp.end())
+                count += mp[sum - k];
+
+            mp[sum]++;
+        }
+
+        return count;
+    }
+};
+
+int main() {
+    Solution obj;
+    vector<int> nums = {1,1,1};
+    int k = 2;
+
+    cout << obj.subarraySum(nums, k);
+}
