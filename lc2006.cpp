ff64106c8e797dc283b7667b8ab496b1a5ab82ae
rohit1302 <rohitnew1302@gmail.com>
2026-03-09 22:29:53 +0530

    Count Number of Pairs With Absolute Difference K

---
+++ lc2006.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int countKDifference(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
+        int ans = 0;
+
+        for(int x : nums) {
+            ans += mp[x-k] + mp[x+k];
+            mp[x]++;
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+    Solution obj;
+
+    vector<int> nums = {1,2,2,1};
+    int k = 1;
+
+    cout << obj.countKDifference(nums, k);
+
+    return 0;
+}
