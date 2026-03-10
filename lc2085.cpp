5b9cb80bd5be6755c8a4ac779a793daa8a81eb96
rohit1302 <rohitnew1302@gmail.com>
2026-03-10 21:59:00 +0530

    Count Common Words With One Occurrence

---
+++ lc2085.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int countWords(vector<string>& w1, vector<string>& w2) {
+        unordered_map<string,int> a,b;
+        int ans = 0;
+
+        for(auto &s : w1) a[s]++;
+        for(auto &s : w2) b[s]++;
+
+        for(auto &p : a)
+            if(p.second == 1 && b[p.first] == 1)
+                ans++;
+
+        return ans;
+    }
+};
+
+int main() {
+    Solution obj;
+
+    vector<string> w1 = {"leetcode","is","amazing","as","is"};
+    vector<string> w2 = {"amazing","leetcode","is"};
+
+    cout << obj.countWords(w1, w2);
+
+    return 0;
+}
