7967673f0222e829816834e0ab77cad805d46e0f
rohit1302 <rohitnew1302@gmail.com>
2026-03-14 23:27:15 +0530

    LeetCode 1358 - Number of Substrings Containing All Three Characters

--- lc1358.cpp
+++ lc1358.cpp
@@ -11,16 +11,13 @@ public:
     int numberOfSubstrings(string s) {
         int last[3] = {-1, -1, -1};
         int ans = 0;
-
         for (int i = 0; i < s.size(); i++) {
             last[s[i] - 'a'] = i;
             ans += 1 + min({last[0], last[1], last[2]});
         }
-
         return ans;
     }
 };
-
 int main() {
     Solution obj;
     string s = "abcabc";
