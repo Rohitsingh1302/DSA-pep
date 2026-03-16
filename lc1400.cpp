d3cfb88364ec9e2b214d24d71ebc9659e5fb32e2
rohit1302 <rohitnew1302@gmail.com>
2026-03-16 22:39:41 +0530

    LeetCode 1400 - Construct K Palindrome Strings

---
+++ lc1400.cpp
@@ -0,0 +1,37 @@
+// LeetCode 1400 - Construct K Palindrome Strings
+// Git: git commit -m "LeetCode 1400 - Construct K Palindrome Strings"
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool canConstruct(string s, int k) {
+        if(k > s.size()) return false;
+
+        vector<int> f(26,0);
+        for(char c : s)
+            f[c-'a']++;
+
+        int odd = 0;
+        for(int x : f)
+            if(x % 2) odd++;
+
+        return odd <= k;
+    }
+};
+
+int main(){
+    string s = "annabelle";
+    int k = 2;
+
+    Solution obj;
+
+    if(obj.canConstruct(s,k))
+        cout<<"true";
+    else
+        cout<<"false";
+
+    return 0;
+}
