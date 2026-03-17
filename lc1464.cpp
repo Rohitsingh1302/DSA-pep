6709fc02ce3ad6cca9312d37b1d3f60b3c0842e9
rohit1302 <rohitnew1302@gmail.com>
2026-03-17 23:31:31 +0530

    LeetCode 1464 - Maximum Product of Two Elements

--- lc1464.cpp
+++ lc1464.cpp
@@ -1,15 +1,10 @@
-// LeetCode 1464 - Maximum Product of Two Elements in an Array
-// Git: git commit -m "LeetCode 1464 - Maximum Product of Two Elements"
-
 #include <iostream>
 #include <vector>
 using namespace std;
-
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         int a = 0, b = 0;
-
         for(int x : nums){
             if(x > a){
                 b = a;
@@ -21,12 +16,9 @@ public:
         return (a-1)*(b-1);
     }
 };
-
 int main(){
     vector<int> nums = {3,4,5,2};
-
     Solution obj;
     cout << obj.maxProduct(nums);
-
     return 0;
 }
