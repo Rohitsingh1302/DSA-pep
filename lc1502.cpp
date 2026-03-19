7aeee9827b704fbb452a0f894922b97673bc26e4
rohit1302 <rohitnew1302@gmail.com>
2026-03-19 23:04:15 +0530

    LeetCode 1502 - Can Make Arithmetic Progression From Sequence

---
+++ lc1502.cpp
@@ -0,0 +1,34 @@
+// LeetCode 1502 - Can Make Arithmetic Progression From Sequence
+// Git: git commit -m "LeetCode 1502 - Arithmetic Progression Check"
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        int d = arr[1] - arr[0];
+
+        for(int i = 2; i < arr.size(); i++)
+            if(arr[i] - arr[i-1] != d)
+                return false;
+
+        return true;
+    }
+};
+
+int main(){
+    vector<int> arr = {3,5,1};
+
+    Solution obj;
+
+    if(obj.canMakeArithmeticProgression(arr))
+        cout << "true";
+    else
+        cout << "false";
+
+    return 0;
+}
