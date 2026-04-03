b073f887a76176c1fec2ff171ed0c56a553f9846
rohit1302 <rohitnew1302@gmail.com>
2026-04-03 23:56:30 +0530

    done with 1748

---
+++ lcc1748.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int sumOfUnique(vector<int>& a) {
+    unordered_map<int,int> m;
+    for(int x:a) m[x]++;
+        int s=0;
+    for(auto &p:m) if(p.second==1) s+=p.first;
+    return s;
+}
+
+int main() {
+    vector<int> a = {1,2,3,2};
+    cout << sumOfUnique(a);
+    return 0;
+}
