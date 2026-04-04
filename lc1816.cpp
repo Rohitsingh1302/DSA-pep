5f7b099bfc866165a31cd23a07ab5b95b443f266
rohit1302 <rohitnew1302@gmail.com>
2026-04-04 22:18:27 +0530

    done with 1816

---
+++ lc1816.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string truncateSentence(string s, int k) {
+    int cnt=0;
+    for(int i=0;i<s.size();i++){
+        if(s[i]==' ') cnt++;
+        if(cnt==k) return s.substr(0,i);
+    }
+    return s;
+}
+
+int main() {
+    string s = "Hello how are you Contestant";
+    int k = 4;
+    cout << truncateSentence(s,k);
+    return 0;
+}
