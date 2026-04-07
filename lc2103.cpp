62d7baa75888e7799f2c0e7bc33cdbb10462872d
rohit1302 <rohitnew1302@gmail.com>
2026-04-07 23:00:35 +0530

    done with 2103

---
+++ lc2103.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int countPoints(string r) {
+    vector<set<char>> v(10);
+    for(int i=0;i<r.size();i+=2)
+        v[r[i+1]-'0'].insert(r[i]);
+    int ans=0;
+    for(auto &s:v) if(s.size()==3) ans++;
+    return ans;
+}
+
+int main() {
+    string r = "B0B6G0R6R0R6G9";
+    cout << countPoints(r);
+    return 0;
+}
