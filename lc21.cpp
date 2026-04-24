dd1904f41167d9eb31d291ddd6c8a163d77975bb
rohit1302 <rohitnew1302@gmail.com>
2026-04-24 23:01:27 +0530

    changes

--- lc21.cpp
+++ lc21.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
 using namespace std;
-
 struct ListNode {
     int val;
     ListNode* next;
     ListNode(int x) : val(x), next(NULL) {}
 };
-
 ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
     ListNode dummy(0);
     ListNode* tail = &dummy;
-
     while (l1 && l2) {
         if (l1->val < l2->val) {
             tail->next = l1;
