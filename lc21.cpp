ed363fd4f47d854125be19303805a8294e81ef2b
rohit1302 <rohitnew1302@gmail.com>
2026-04-26 23:49:39 +0530

    cncised code

--- lc21.cpp
+++ lc21.cpp
@@ -1,43 +1,23 @@
 #include <iostream>
-using namespace std;
-struct ListNode {
-    int val;
-    ListNode* next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
-ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-    ListNode dummy(0);
-    ListNode* tail = &dummy;
-    while (l1 && l2) {
-        if (l1->val < l2->val) {
-            tail->next = l1;
-            l1 = l1->next;
-        } else {
-            tail->next = l2;
-            l2 = l2->next;
-        }
-        tail = tail->next;
-    }
-    if (l1) tail->next = l1;
-    if (l2) tail->next = l2;
 
-    return dummy.next;
+struct Node {
+    int val;
+    Node* next;
+    Node(int v, Node* n = nullptr) : val(v), next(n) {}
+};
+
+Node* merge(Node* l1, Node* l2) {
+    if (!l1 || !l2) return l1 ? l1 : l2;
+    if (l1->val < l2->val) return l1->next = merge(l1->next, l2), l1;
+    return l2->next = merge(l1, l2->next), l2;
 }
-void printList(ListNode* head) {
-    while (head) {
-        cout << head->val << " ";
-        head = head->next;
-    }
+
+void print(Node* head) {
+    for (; head; head = head->next) std::cout << head->val << " ";
 }
+
 int main() {
-    // List 1: 1->3->5
-    ListNode* l1 = new ListNode(1);
-    l1->next = new ListNode(3);
-    l1->next->next = new ListNode(5);
-    // List 2: 2->4->6
-    ListNode* l2 = new ListNode(2);
-    l2->next = new ListNode(4);
-    l2->next->next = new ListNode(6);
-    ListNode* merged = mergeTwoLists(l1, l2);
-    printList(merged);  // Output: 1 2 3 4 5 6
+    Node* l1 = new Node(1, new Node(3, new Node(5)));
+    Node* l2 = new Node(2, new Node(4, new Node(6)));
+    print(merge(l1, l2));
 }
