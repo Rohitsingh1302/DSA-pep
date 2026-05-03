b7eb10e13a83c73105e15054e837ec7209b3a873
rohit1302 <rohitnew1302@gmail.com>
2026-05-03 23:45:35 +0530

    changes

--- lc21.cpp
+++ lc21.cpp
@@ -5,13 +5,11 @@ struct Node {
     Node* next;
     Node(int v, Node* n = nullptr) : val(v), next(n) {}
 };
-
 Node* merge(Node* l1, Node* l2) {
     if (!l1 || !l2) return l1 ? l1 : l2;
     if (l1->val < l2->val) return l1->next = merge(l1->next, l2), l1;
     return l2->next = merge(l1, l2->next), l2;
 }
-
 void print(Node* head) {
     for (; head; head = head->next) std::cout << head->val << " ";
 }
