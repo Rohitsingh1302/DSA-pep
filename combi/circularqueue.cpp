b8b356b7caba9920647a02d9a96cf830bfc02a00
rohit1302 <rohitnew1302@gmail.com>
2026-05-13 10:59:05 +0530

    circular que implementation

--- combi/circularqueue.cpp
+++ combi/circularqueue.cpp
@@ -43,4 +43,17 @@ public:
             front=(front+1)%size;
         }
     }
+    int main(){
+        CircularQueue q;
+        q.enqueue(1);
+        q.enqueue(2);
+        q.enqueue(3);
+        q.enqueue(4);
+        q.enqueue(5);
+        q.dequeue();
+        q.dequeue();
+        q.enqueue(6);
+        q.enqueue(7);
+        return 0;
+    }
 };
