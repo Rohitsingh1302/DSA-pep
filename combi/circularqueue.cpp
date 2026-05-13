689f3970f6f83176799e1037553cdaf1aee25e20
rohit1302 <rohitnew1302@gmail.com>
2026-05-13 10:58:16 +0530

    enque-deque

---
+++ combi/circularqueue.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+class CircularQueue {
+    int rear;
+    int size;
+    int front;
+    int *queue;
+public:
+    CircularQueue(){
+        front=-1;
+        rear=-1;
+        cout<<"Enter the size of the queue: ";
+        cin>>size;
+        queue=new int[size];
+        cout<<"Queue of size "<<size<<" created successfully!"<<endl;
+    }
+    void enqueue(int data){
+        if ((front==0 && rear==size-1) || (rear+1==front)){
+            cout<<"overflow!!"<<endl;
+        }
+        else if(front==-1 && rear==-1){
+            front=rear=0;
+            queue[rear]=data;
+            cout<<"Enqueued element successfully!!: "<<data<<endl;
+        }
+        else{
+            rear=(rear+1)%size;
+            queue[rear]=data;
+            cout<<"Enqueued element successfully!!: "<<data<<endl;
+        }
+    }
+    void dequeue(){
+        if(front==-1){
+            cout<<"Underflow!!"<<endl;
+        }
+        else if(front==rear){
+            cout<<"Dequeued element successfully!!: "<<queue[front]<<endl;
+            front=rear=-1;
+        }
+        else{
+            cout<<"Dequeued element successfully!!: "<<queue[front]<<endl;
+            front=(front+1)%size;
+        }
+    }
+};
