7f7c92438746f4ebed185b4df5b4d3d17b7fdc3a
rohit1302 <rohitnew1302@gmail.com>
2026-07-05 22:37:24 +0530

    pow pf three

--- array/lc326.cpp
+++ array/lc326.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 using namespace std;
-
 class Solution {
 public:
     bool isPowerOfThree(int n) {
@@ -9,23 +8,18 @@ public:
         while (n % 3 == 0) {
             n = n / 3;
         }
-
         return n == 1;
     }
 };
-
 int main() {
     Solution sol;
     int n;
-    
     cout << "Enter a number: ";
     cin >> n;
-    
     if (sol.isPowerOfThree(n)) {
         cout << n << " is a power of three." << endl;
     } else {
         cout << n << " is not a power of three." << endl;
     }
-    
     return 0;
 }
