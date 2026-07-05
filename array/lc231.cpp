4ab213517827d3e7c1b38c954f6b54431a24aa1f
rohit1302 <rohitnew1302@gmail.com>
2026-07-05 22:34:34 +0530

    fibonacci edit

--- array/lc231.cpp
+++ array/lc231.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 using namespace std;
-
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
@@ -8,19 +7,15 @@ public:
         return (n & (n - 1)) == 0;
     }
 };
-
 int main() {
     Solution sol;
     int n;
-    
     cout << "Enter a number: ";
     cin >> n;
-    
     if (sol.isPowerOfTwo(n)) {
         cout << n << " is a power of two." << endl;
     } else {
         cout << n << " is not a power of two." << endl;
     }
-    
     return 0;
 }
